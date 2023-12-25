24a1ca5e55025853a7c0985e9a17a7c4080cb18e
Pavel <palllkaignatev@gmail.com>
2023-12-25 19:35:44 +0400

    Обновление .gitignore

--- api.js
+++ api.js
@@ -188,6 +188,7 @@ async function processCompleteRequest(req, res) {
       top_p: 1,
       frequency_penalty: 0.3,
       presence_penalty: 1,
+      stop: ["#"],
     };
 
     const response = await fetch(
@@ -214,7 +215,8 @@ async function processCompleteRequest(req, res) {
       !responseData.usage.completion_tokens ||
       !responseData.choices ||
       !responseData.choices[0] ||
-      !responseData.choices[0].text
+      !responseData.choices[0].text ||
+      responseData.choices[0].text.length < 30
     ) {
       throw new Error("Result not found");
     }
